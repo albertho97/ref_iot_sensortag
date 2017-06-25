0393a29bc9febc26f99ac137278e7f3f7e9603bd
albertho97 <albert@cbaworldnz.com>
2017-06-25 12:58:18 +0800

    update

--- raspberry-readings-worker/push_iot_all.sh
+++ raspberry-readings-worker/push_iot_all.sh
@@ -1,5 +1,2 @@
-sudo hciconfig hci0 up
-bash /home/pi/bluepy/bluepy/push_iot_1.sh &
-bash /home/pi/bluepy/bluepy/push_iot_2.sh &
-bash /home/pi/bluepy/bluepy/push_iot_3.sh &
-bash /home/pi/bluepy/bluepy/push_iot_4.sh
+#sudo hciconfig hci0 up
+bash /home/pi/bluepy/bluepy/push_iot_1.sh & bash /home/pi/bluepy/bluepy/push_iot_2.sh & bash /home/pi/bluepy/bluepy/push_iot_3.sh & bash /home/pi/bluepy/bluepy/push_iot_4.sh
